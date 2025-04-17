a87b6113a6bea9ae68ffdd85f859f076ddf559b8
juandi247 <jdiaz873@unab.edu.co>
2025-04-17 18:22:05 -0500

    feat: detect empty application slot to auto-enter USB boot mode

--- bootloader.c
+++ bootloader.c
@@ -20,142 +20,150 @@
  * SOFTWARE.
  */
 
-#include <stdio.h>
-#include <string.h>
+ #include <stdio.h>
+ #include <string.h>
+ #include "pico/bootrom.h"
+ 
+ #include <RP2040.h>
+ #include <hardware/flash.h>
+ #include <hardware/resets.h>
+ #include <hardware/sync.h>
+ #include <pico/stdlib.h>
+ 
+ #include <pico_fota_bootloader.h>
+ 
+ #include "linker_common/linker_definitions.h"
+ 
+ #ifdef PFB_WITH_BOOTLOADER_LOGS
+ #    define BOOTLOADER_LOG(...)                \
+         do {                                   \
+             puts("[BOOTLOADER] " __VA_ARGS__); \
+             sleep_ms(5);                       \
+         } while (0)
+ #else // PFB_WITH_BOOTLOADER_LOGS
+ #    define BOOTLOADER_LOG(...) ((void) 0)
+ #endif // PFB_WITH_BOOTLOADER_LOGS
+ 
+ void _pfb_mark_pico_has_new_firmware(void);
+ void _pfb_mark_pico_has_no_new_firmware(void);
+ void _pfb_mark_is_after_rollback(void);
+ void _pfb_mark_is_not_after_rollback(void);
+ bool _pfb_should_rollback(void);
+ void _pfb_mark_should_rollback(void);
+ bool _pfb_has_firmware_to_swap(void);
+ 
+ static void swap_images(void) {
+     uint8_t swap_buff_from_downlaod_slot[FLASH_SECTOR_SIZE];
+     uint8_t swap_buff_from_application_slot[FLASH_SECTOR_SIZE];
+     const uint32_t SWAP_ITERATIONS =
+             PFB_ADDR_AS_U32(__FLASH_SWAP_SPACE_LENGTH) / FLASH_SECTOR_SIZE;
+ 
+     uint32_t saved_interrupts = save_and_disable_interrupts();
+     for (uint32_t i = 0; i < SWAP_ITERATIONS; i++) {
+         memcpy(swap_buff_from_downlaod_slot,
+                (void *) (PFB_ADDR_AS_U32(__FLASH_DOWNLOAD_SLOT_START)
+                          + i * FLASH_SECTOR_SIZE),
+                FLASH_SECTOR_SIZE);
+         memcpy(swap_buff_from_application_slot,
+                (void *) (PFB_ADDR_AS_U32(__FLASH_APP_START)
+                          + i * FLASH_SECTOR_SIZE),
+                FLASH_SECTOR_SIZE);
+         flash_range_erase(PFB_ADDR_WITH_XIP_OFFSET_AS_U32(__FLASH_APP_START)
+                                   + i * FLASH_SECTOR_SIZE,
+                           FLASH_SECTOR_SIZE);
+         flash_range_erase(PFB_ADDR_WITH_XIP_OFFSET_AS_U32(
+                                   __FLASH_DOWNLOAD_SLOT_START)
+                                   + i * FLASH_SECTOR_SIZE,
+                           FLASH_SECTOR_SIZE);
+         flash_range_program(PFB_ADDR_WITH_XIP_OFFSET_AS_U32(__FLASH_APP_START)
+                                     + i * FLASH_SECTOR_SIZE,
+                             swap_buff_from_downlaod_slot,
+                             FLASH_SECTOR_SIZE);
+         flash_range_program(PFB_ADDR_WITH_XIP_OFFSET_AS_U32(
+                                     __FLASH_DOWNLOAD_SLOT_START)
+                                     + i * FLASH_SECTOR_SIZE,
+                             swap_buff_from_application_slot,
+                             FLASH_SECTOR_SIZE);
+     }
+     restore_interrupts(saved_interrupts);
+ }
+ 
+ // Added a _ prefix to avoid name collision with the pico-sdk 2.1.1
+ static void _disable_interrupts(void) {
+     SysTick->CTRL &= ~1;
+ 
+     NVIC->ICER[0] = 0xFFFFFFFF;
+     NVIC->ICPR[0] = 0xFFFFFFFF;
+ }
+ 
+ static void reset_peripherals(void) {
+     reset_block(~(RESETS_RESET_IO_QSPI_BITS | RESETS_RESET_PADS_QSPI_BITS
+                   | RESETS_RESET_SYSCFG_BITS | RESETS_RESET_PLL_SYS_BITS));
+ }
+ 
+ static void jump_to_vtor(uint32_t vtor) {
+     // Derived from the Leaf Labs Cortex-M3 bootloader.
+     // Copyright (c) 2010 LeafLabs LLC.
+     // Modified 2021 Brian Starkey <stark3y@gmail.com>
+     // Originally under The MIT License
+ 
+     uint32_t reset_vector = *(volatile uint32_t *) (vtor + 0x04);
+     SCB->VTOR = (volatile uint32_t)(vtor);
+ 
+     asm volatile("msr msp, %0" ::"g"(*(volatile uint32_t *) vtor));
+     asm volatile("bx %0" ::"r"(reset_vector));
+ }
+ 
+ static void print_welcome_message(void) {
+ #ifdef PFB_WITH_BOOTLOADER_LOGS
+     puts("");
+     puts("***********************************************************");
+     puts("*                                                         *");
+     puts("*           Raspberry Pi Pico W FOTA Bootloader           *");
+     puts("*             Copyright (c) 2024 Jakub Zimnol             *");
+     puts("*                                                         *");
+     puts("***********************************************************");
+     puts("");
+ #endif // PFB_WITH_BOOTLOADER_LOGS
+ }
+ 
+ int main(void) {
+     stdio_init_all();
+     sleep_ms(2000);
+ 
+     print_welcome_message();
+ 
+     if (_pfb_should_rollback()) {
+         BOOTLOADER_LOG("Rolling back to the previous firmware");
+         swap_images();
+         pfb_firmware_commit();
+         _pfb_mark_pico_has_no_new_firmware();
+         _pfb_mark_is_after_rollback();
+     } else if (_pfb_has_firmware_to_swap()) {
+         BOOTLOADER_LOG("Swapping images");
+         swap_images();
+         _pfb_mark_pico_has_new_firmware();
+         _pfb_mark_is_not_after_rollback();
+         _pfb_mark_should_rollback();
+     } else {
+         BOOTLOADER_LOG("Nothing to swap");
+         pfb_firmware_commit();
+         _pfb_mark_pico_has_no_new_firmware();
+     }
+ 
+     pfb_mark_download_slot_as_invalid();
 
-#include <RP2040.h>
-#include <hardware/flash.h>
-#include <hardware/resets.h>
-#include <hardware/sync.h>
-#include <pico/stdlib.h>
-
-#include <pico_fota_bootloader.h>
-
-#include "linker_common/linker_definitions.h"
-
-#ifdef PFB_WITH_BOOTLOADER_LOGS
-#    define BOOTLOADER_LOG(...)                \
-        do {                                   \
-            puts("[BOOTLOADER] " __VA_ARGS__); \
-            sleep_ms(5);                       \
-        } while (0)
-#else // PFB_WITH_BOOTLOADER_LOGS
-#    define BOOTLOADER_LOG(...) ((void) 0)
-#endif // PFB_WITH_BOOTLOADER_LOGS
-
-void _pfb_mark_pico_has_new_firmware(void);
-void _pfb_mark_pico_has_no_new_firmware(void);
-void _pfb_mark_is_after_rollback(void);
-void _pfb_mark_is_not_after_rollback(void);
-bool _pfb_should_rollback(void);
-void _pfb_mark_should_rollback(void);
-bool _pfb_has_firmware_to_swap(void);
-
-static void swap_images(void) {
-    uint8_t swap_buff_from_downlaod_slot[FLASH_SECTOR_SIZE];
-    uint8_t swap_buff_from_application_slot[FLASH_SECTOR_SIZE];
-    const uint32_t SWAP_ITERATIONS =
-            PFB_ADDR_AS_U32(__FLASH_SWAP_SPACE_LENGTH) / FLASH_SECTOR_SIZE;
-
-    uint32_t saved_interrupts = save_and_disable_interrupts();
-    for (uint32_t i = 0; i < SWAP_ITERATIONS; i++) {
-        memcpy(swap_buff_from_downlaod_slot,
-               (void *) (PFB_ADDR_AS_U32(__FLASH_DOWNLOAD_SLOT_START)
-                         + i * FLASH_SECTOR_SIZE),
-               FLASH_SECTOR_SIZE);
-        memcpy(swap_buff_from_application_slot,
-               (void *) (PFB_ADDR_AS_U32(__FLASH_APP_START)
-                         + i * FLASH_SECTOR_SIZE),
-               FLASH_SECTOR_SIZE);
-        flash_range_erase(PFB_ADDR_WITH_XIP_OFFSET_AS_U32(__FLASH_APP_START)
-                                  + i * FLASH_SECTOR_SIZE,
-                          FLASH_SECTOR_SIZE);
-        flash_range_erase(PFB_ADDR_WITH_XIP_OFFSET_AS_U32(
-                                  __FLASH_DOWNLOAD_SLOT_START)
-                                  + i * FLASH_SECTOR_SIZE,
-                          FLASH_SECTOR_SIZE);
-        flash_range_program(PFB_ADDR_WITH_XIP_OFFSET_AS_U32(__FLASH_APP_START)
-                                    + i * FLASH_SECTOR_SIZE,
-                            swap_buff_from_downlaod_slot,
-                            FLASH_SECTOR_SIZE);
-        flash_range_program(PFB_ADDR_WITH_XIP_OFFSET_AS_U32(
-                                    __FLASH_DOWNLOAD_SLOT_START)
-                                    + i * FLASH_SECTOR_SIZE,
-                            swap_buff_from_application_slot,
-                            FLASH_SECTOR_SIZE);
-    }
-    restore_interrupts(saved_interrupts);
-}
-
-// Added a _ prefix to avoid name collision with the pico-sdk 2.1.1
-static void _disable_interrupts(void) {
-    SysTick->CTRL &= ~1;
-
-    NVIC->ICER[0] = 0xFFFFFFFF;
-    NVIC->ICPR[0] = 0xFFFFFFFF;
-}
-
-static void reset_peripherals(void) {
-    reset_block(~(RESETS_RESET_IO_QSPI_BITS | RESETS_RESET_PADS_QSPI_BITS
-                  | RESETS_RESET_SYSCFG_BITS | RESETS_RESET_PLL_SYS_BITS));
-}
-
-static void jump_to_vtor(uint32_t vtor) {
-    // Derived from the Leaf Labs Cortex-M3 bootloader.
-    // Copyright (c) 2010 LeafLabs LLC.
-    // Modified 2021 Brian Starkey <stark3y@gmail.com>
-    // Originally under The MIT License
-
-    uint32_t reset_vector = *(volatile uint32_t *) (vtor + 0x04);
-    SCB->VTOR = (volatile uint32_t)(vtor);
-
-    asm volatile("msr msp, %0" ::"g"(*(volatile uint32_t *) vtor));
-    asm volatile("bx %0" ::"r"(reset_vector));
-}
-
-static void print_welcome_message(void) {
-#ifdef PFB_WITH_BOOTLOADER_LOGS
-    puts("");
-    puts("***********************************************************");
-    puts("*                                                         *");
-    puts("*           Raspberry Pi Pico W FOTA Bootloader           *");
-    puts("*             Copyright (c) 2024 Jakub Zimnol             *");
-    puts("*                                                         *");
-    puts("***********************************************************");
-    puts("");
-#endif // PFB_WITH_BOOTLOADER_LOGS
-}
-
-int main(void) {
-    stdio_init_all();
-    sleep_ms(2000);
-
-    print_welcome_message();
-
-    if (_pfb_should_rollback()) {
-        BOOTLOADER_LOG("Rolling back to the previous firmware");
-        swap_images();
-        pfb_firmware_commit();
-        _pfb_mark_pico_has_no_new_firmware();
-        _pfb_mark_is_after_rollback();
-    } else if (_pfb_has_firmware_to_swap()) {
-        BOOTLOADER_LOG("Swapping images");
-        swap_images();
-        _pfb_mark_pico_has_new_firmware();
-        _pfb_mark_is_not_after_rollback();
-        _pfb_mark_should_rollback();
-    } else {
-        BOOTLOADER_LOG("Nothing to swap");
-        pfb_firmware_commit();
-        _pfb_mark_pico_has_no_new_firmware();
-    }
-
-    pfb_mark_download_slot_as_invalid();
-    BOOTLOADER_LOG("End of execution, executing the application...\n");
-
-    _disable_interrupts();
-    reset_peripherals();
-    jump_to_vtor(__flash_info_app_vtor);
-
-    return 0;
-}
+     if (pfb_is_application_slot_empty()) {
+         sleep_ms(4000);
+         reset_usb_boot(0, 0); 
+     }
+     
+     BOOTLOADER_LOG("End of execution, executing the application...\n");
+ 
+     _disable_interrupts();
+     reset_peripherals();
+     jump_to_vtor(__flash_info_app_vtor);
+ 
+     return 0;
+ }
+ 
--- include/pico_fota_bootloader.h
+++ include/pico_fota_bootloader.h
@@ -20,114 +20,135 @@
  * SOFTWARE.
  */
 
-#ifndef PICO_FOTA_BOOTLOADER_H
-#define PICO_FOTA_BOOTLOADER_H
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include <pico/stdlib.h>
-
-#define PFB_ALIGN_SIZE (256)
-
-/**
- * Marks the download slot as valid, i.e. download slot contains proper binary
- * content and the partitions can be swapped. MUST be called before the next
- * reboot, otherwise data from the download slot will be lost.
- */
-void pfb_mark_download_slot_as_valid(void);
-
-/**
- * Marks the download slot as invalid, i.e. download slot no longer contains
- * proper binary content and the partitions MUST NOT be swapped.
- */
-void pfb_mark_download_slot_as_invalid(void);
-
-/**
- * Returns the information if the executed app comes from the download slot.
- * NOTE: this function will return true only if the firmware update has been
- *       executed during the previous reboot.
- *
- * @return true if the partitions were swapped during the previous reboot,
- *         false otherwise.
- */
-bool pfb_is_after_firmware_update(void);
-
-/**
- * Writes data into the download partition and checks, if the length of the data
- * is 256 bytes alligned.
- * If @ref PFB_WITH_IMAGE_ENCRYPTION is defined, the function will decrypt the
- * downloaded data using the PFB_AES_KEY.
- *
- * @param src          Pointer to the source buffer.
- * @param offset_bytes Offset which should be applied to the beginning of the
- *                     download slot partition.
- * @param len_bytes    Number of bytes that should be written into flash. MUST
- *                     be a multiple of 256.
- *
- * @return 1 when @p len_bytes or @p offset_bytes are not multiple of 256 or
- *         when ( @p offset_bytes + @p len_bytes ) exceeds download slot size,
- *         negative mbedtls error code in case of an error if
- *         @ref PFB_WITH_IMAGE_ENCRYPTION is defined,
- *         0 otherwise.
- */
-int pfb_write_to_flash_aligned_256_bytes(uint8_t *src,
-                                         size_t offset_bytes,
-                                         size_t len_bytes);
-
-/**
- * Initializes the download slot, i.e. erases the download partition. MUST be
- * called before writing data into the flash. Before an erase, the function will
- * call @ref pfb_firmware_commit even if @ref pfb_firmware_commit has been
- * called before.
- *
- * @return mbedtls error code in case of a mbedtls error if
- *         @ref PFB_WITH_IMAGE_ENCRYPTION is defined,
- *         0 otherwise.
- */
-int pfb_initialize_download_slot(void);
-
-/**
- * Performs the firmware update. Reboots the Pico and checks if the partitions
- * should be swapped.
- */
-void pfb_perform_update(void);
-
-/**
- * Marks the information that the device SHOULD NOT perform rollback in case of
- * a reboot.
- */
-void pfb_firmware_commit(void);
-
-/**
- * Returns the information if the device has performed a rollback during the
- * reboot.
- * NOTE: this function will return true only if the rollback has been performed
- *       during the very previous reboot.
- *
- * @return true if the rollback has been performed during the previous reboot,
- *         false otherwise.
- */
-bool pfb_is_after_rollback(void);
-
-/**
- * If @ref WITH_SHA256 is defined, checks if the calculated SHA256 of the image
- * matches the expected one. Otherwise, the function will only return 0.
- *
- * @param firmware_size Size of the downloaded firmware image in bytes.
- *
- * @return A negative mbedtls error code on calculation error,
- *         1 if the firmware size is not a multiple of 256 or if the calculated
- *         and expected sha are different,
- *         0 otherwise.
- *         If @ref PFB_WITH_SHA256_HASHING is not defined, the function will
- *         always return 0.
- */
-int pfb_firmware_sha256_check(size_t firmware_size);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif // PICO_FOTA_BOOTLOADER_H
+ #ifndef PICO_FOTA_BOOTLOADER_H
+ #define PICO_FOTA_BOOTLOADER_H
+ 
+ #ifdef __cplusplus
+ extern "C" {
+ #endif
+ 
+ #include <pico/stdlib.h>
+ 
+ #define PFB_ALIGN_SIZE (256)
+ 
+ /**
+  * Marks the download slot as valid, i.e. download slot contains proper binary
+  * content and the partitions can be swapped. MUST be called before the next
+  * reboot, otherwise data from the download slot will be lost.
+  */
+ void pfb_mark_download_slot_as_valid(void);
+ 
+ /**
+  * Marks the download slot as invalid, i.e. download slot no longer contains
+  * proper binary content and the partitions MUST NOT be swapped.
+  */
+ void pfb_mark_download_slot_as_invalid(void);
+ 
+ /**
+  * Returns the information if the executed app comes from the download slot.
+  * NOTE: this function will return true only if the firmware update has been
+  *       executed during the previous reboot.
+  *
+  * @return true if the partitions were swapped during the previous reboot,
+  *         false otherwise.
+  */
+ bool pfb_is_after_firmware_update(void);
+ 
+ /**
+  * Writes data into the download partition and checks, if the length of the data
+  * is 256 bytes alligned.
+  * If @ref PFB_WITH_IMAGE_ENCRYPTION is defined, the function will decrypt the
+  * downloaded data using the PFB_AES_KEY.
+  *
+  * @param src          Pointer to the source buffer.
+  * @param offset_bytes Offset which should be applied to the beginning of the
+  *                     download slot partition.
+  * @param len_bytes    Number of bytes that should be written into flash. MUST
+  *                     be a multiple of 256.
+  *
+  * @return 1 when @p len_bytes or @p offset_bytes are not multiple of 256 or
+  *         when ( @p offset_bytes + @p len_bytes ) exceeds download slot size,
+  *         negative mbedtls error code in case of an error if
+  *         @ref PFB_WITH_IMAGE_ENCRYPTION is defined,
+  *         0 otherwise.
+  */
+ int pfb_write_to_flash_aligned_256_bytes(uint8_t *src,
+                                          size_t offset_bytes,
+                                          size_t len_bytes);
+ 
+ /**
+  * Initializes the download slot, i.e. erases the download partition. MUST be
+  * called before writing data into the flash. Before an erase, the function will
+  * call @ref pfb_firmware_commit even if @ref pfb_firmware_commit has been
+  * called before.
+  *
+  * @return mbedtls error code in case of a mbedtls error if
+  *         @ref PFB_WITH_IMAGE_ENCRYPTION is defined,
+  *         0 otherwise.
+  */
+ int pfb_initialize_download_slot(void);
+ 
+ /**
+  * Performs the firmware update. Reboots the Pico and checks if the partitions
+  * should be swapped.
+  */
+ void pfb_perform_update(void);
+ 
+ /**
+  * Marks the information that the device SHOULD NOT perform rollback in case of
+  * a reboot.
+  */
+ void pfb_firmware_commit(void);
+ 
+ /**
+  * Returns the information if the device has performed a rollback during the
+  * reboot.
+  * NOTE: this function will return true only if the rollback has been performed
+  *       during the very previous reboot.
+  *
+  * @return true if the rollback has been performed during the previous reboot,
+  *         false otherwise.
+  */
+ bool pfb_is_after_rollback(void);
+ 
+ /**
+  * If @ref WITH_SHA256 is defined, checks if the calculated SHA256 of the image
+  * matches the expected one. Otherwise, the function will only return 0.
+  *
+  * @param firmware_size Size of the downloaded firmware image in bytes.
+  *
+  * @return A negative mbedtls error code on calculation error,
+  *         1 if the firmware size is not a multiple of 256 or if the calculated
+  *         and expected sha are different,
+  *         0 otherwise.
+  *         If @ref PFB_WITH_SHA256_HASHING is not defined, the function will
+  *         always return 0.
+  */
+ int pfb_firmware_sha256_check(size_t firmware_size);
+ 
+ /**
+  * Checks whether the application slot (i.e., the main firmware region)
+  * is empty or invalid.
+  *
+  * This function inspects the reset handler address (located at offset +4 
+  * from the application start address) to determine whether a valid 
+  * firmware is present. It returns true if the reset handler is:
+  * - 0xFFFFFFFF (uninitialized flash),
+  * - Below 0x10000000 (outside valid memory space),
+  * - Above 0x10200000 (out of flash range for RP2040).
+  *
+  * This check is especially useful in custom boards without BOOTSEL buttons,
+  * allowing the firmware to detect an empty slot and trigger a USB reset
+  * to enter BOOTLOADER mode automatically.
+  *
+  * @return true if the application slot is empty or contains an invalid
+  *         firmware, false otherwise.
+  */
+ bool pfb_is_application_slot_empty(void);
+ 
+ #ifdef __cplusplus
+ }
+ #endif
+ 
+ #endif // PICO_FOTA_BOOTLOADER_H
+ 
